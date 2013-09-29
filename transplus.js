dff10eefcb9b70efe286f7dbf679e91d23184dd0
spectrum <spectrick@gmail.com>
2013-09-30 05:08:13 +0900

    IE 지원 XDomainRequest 추가. jsonp 예외 추가

--- transplus.js
+++ transplus.js
@@ -1,13 +1,42 @@
 var interlang = "ja";
 
+var isCORSSupport = 'withCredentials' in new XMLHttpRequest();
+var isIE = typeof XDomainRequest !== "undefined";
+var xdr;
+
+var getJSON = function(query, callback) {
+  if (isCORSSupport) {
+    $.getJSON(query, callback);
+  } else if (isIE) {
+    xdr = new XDomainRequest();
+    if (xdr) {
+      xdr.onload = callback;
+      xdr.open("get", query);
+      xdr.send();
+    }
+  } else {
+    $.ajax({
+      type: "GET",
+      dataType: "jsonp",
+      jsonp: "callback",
+      url: query,
+      success: callback
+    });
+  }
+}
+
 var setQueryString = function(message) {
-  var result = "http://localhost:8080/post?http://translate.google.com/translate_a/t?client=x"+
-    "&sl="+message.userlang+"&tl="+message.targetlang+
-    "&text="+encodeURIComponent(message.text)
+  var result = "http://goxcors.appspot.com/cors?method=GET" +
+    "&url=" + encodeURIComponent("http://translate.google.com/translate_a/t?client=x" +
+    "&sl=" + message.userlang + "&tl=" + message.targetlang+
+    "&text=" + encodeURIComponent(message.text));
   return result;
 };
 
 var extractResult = function(data) {
+  if (!isCORSSupport && isIE) {
+    data = $.parseJSON(data.responseText);
+  }
   return data && data.sentences && $.map(data.sentences, (function(v) { return v.trans })).join('');
 };
 
@@ -21,7 +50,7 @@ var translateInterLang=function(data) {
   message.userlang = interlang;
   message.targetlang = $("#targetLang").val();
   $(".translateJapanResult").text(post);
-  $.getJSON(setQueryString(message, interlang), translateFinalLang);
+  getJSON(setQueryString(message), translateFinalLang);
 };
 
 var translateDirectLang=function(data) {
@@ -35,9 +64,9 @@ $("form").submit(function() {
     userlang: $("#userLang").val(),
     targetlang: $("#targetLang").val()
   };
-  $.getJSON(setQueryString(message), translateDirectLang);
+  getJSON(setQueryString(message), translateDirectLang);
   message.targetlang = interlang;
-  $.getJSON(setQueryString(message), translateInterLang);
+  getJSON(setQueryString(message), translateInterLang);
   return false;
 });
 
